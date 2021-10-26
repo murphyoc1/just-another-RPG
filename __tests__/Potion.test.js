cd6f9dfcbbcf1bec4d457f8c4e58722581149c48
murphyoc1 <murphyoc1@gmail.com>
2021-10-26 19:02:07 -0400

    Added test code

---
+++ __tests__/Potion.test.js
@@ -0,0 +1,6 @@
+test('creates a health potion object', () => {
+    const potion = new Potion('health');
+  
+    expect(potion.name).toBe('health');
+    expect(potion.value).toEqual(expect.any(Number));
+  });
